66e42fc0a9b6625006b907f47722e5c0cb01d3c6
Tabari Alexander <tehbone@gmail.com>
2017-12-23 19:27:42 -0500

    Did day 4 part b.
    Just a one liner - if we are excluding anagrams, then we can store the words
    sorted by letter in our exclusion set. anagrams would be sorted in the same
    way.

---
+++ day4/b.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import sys
+
+DEFAULT_FILENAME='input.txt'
+
+def get_passphrase(filename):
+    with open(filename, 'r') as f:
+        for line in f:
+            l = line.strip()
+            if l:
+                yield l
+
+def is_passphrase_valid(passphrase):
+    s = set()
+    for word in passphrase.split():
+        word = ''.join(sorted(word))
+        if word in s:
+            return False
+        s.add(word)
+
+    return True
+
+def main(filename):
+    c = 0
+    for passphrase in get_passphrase(filename):
+        if is_passphrase_valid(passphrase):
+            c = c + 1
+
+    print('Num valid: {}'.format(c))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = DEFAULT_FILENAME
+
+    main(filename)
