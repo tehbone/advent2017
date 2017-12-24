1215a226d92142a8d78239297db432c02c92f34a
Tabari Alexander <tehbone@gmail.com>
2017-12-23 21:13:31 -0500

    Day 6 part 1 done.
    stringifying the sequence was the best approach to check to see if we hit the
    state already.

---
+++ day6/a.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import sys
+
+def get_max(banks):
+    i = 0
+    v = banks[0]
+    for j in xrange(1, len(banks)):
+        if banks[j] > v:
+            v = banks[j]
+            i = j
+
+    return v, i
+
+def get_banks():
+    with open('input.txt', 'r') as f:
+        return [int(x) for x in f.readline().split()]
+
+def main():
+    cache = set()
+    num_iter = 0
+    if len(sys.argv) > 1:
+        banks = [int(x) for x in sys.argv[1::]]
+    else:
+        banks = get_banks()
+
+    num_banks = len(banks)
+    while True:
+        v, i = get_max(banks)
+        r = v % num_banks
+        c = (v - r) / num_banks
+        banks[i] = 0
+        banks = [ x + c for x in banks]
+        for j in xrange(i + 1, i + 1 + r):
+            k = j % num_banks
+            banks[k] = banks[k] + 1
+
+        key = ' '.join([str(x) for x in banks])
+        num_iter = num_iter + 1
+        if key in cache:
+            break
+
+        cache.add(key)
+
+    print('Number of iterations: {}'.format(num_iter))
+    
+if __name__ == '__main__':
+    main()
