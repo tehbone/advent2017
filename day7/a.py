44a3b54be23c69ec40fb587b481fa77afbdd1a75
Tabari Alexander <tehbone@gmail.com>
2017-12-23 22:53:58 -0500

    Day 7 part 1.
    really all that needed to be done is to check to see which program isn't a
    a child.

---
+++ day7/a.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import sys
+
+class Program(object):
+    def __init__(self, desc):
+        super(Program, self).__init__()
+        i = desc.find(' ')
+        self.name = desc[0:i]
+        i = desc.find('(',i+1)
+        j = desc.find(')',i+1)
+        self.weight = int(desc[i+1:j])
+        i = desc.find('-> ', j+1)
+        if i == -1:
+            self.children = set()
+        else:
+            self.children = set(desc[i+3:].split(', '))
+
+def programs(filename):
+    with open(filename, 'r') as f:
+        for l in f:
+            line = l.strip()
+            if line:
+                yield Program(line)
+
+def main(filename):
+    candidates = []
+    children = set()
+    for prog in programs(filename):
+        candidates.append(prog.name)
+        children = children | prog.children
+
+    for c in candidates:
+        if c not in children:
+            print(c)
+            return
+
+    print('Not found')
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        main('input.txt')
